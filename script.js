52cd905bba16a32310cf294f453dce9e1022190c
Coding Person Ig <66042843+codingpersonig@users.noreply.github.com>
2025-10-21 09:03:49 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function load() {
-    // Get Elements
-    const body = document.body;
-    const menuBtn = document.getElementsByClassName("menuBtn")[0];
-}
